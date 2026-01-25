3b200ebb9e64820a84cee55dab5e4c4ae65c92e1
Jamong-E <jamonglee2323@gmail.com>
2026-01-25 10:28:53 +0900

    123

---
+++ Assets/02.Scripts/NutController.cs
@@ -0,0 +1,17 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class NutController : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        Shoot(new Vector3(0, 200, 2000));
+    }
+
+    public void Shoot(Vector3 dir)
+    {
+        GetComponent<Rigidbody>().AddForce(dir);
+    }
+}
